1f850f299171167ba1c42fb7d74755f4a32bccff
SolidHinken <solidhinken@gmail.com>
2019-01-26 19:07:57 +0300

    26.01.19
    Initial Commit, clicking material changer, Cube prefs

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Material[] materials;
+    public Camera camHolder;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GrabRayObj();
+        }
+    }
+
+
+
+    public void GrabRayObj()
+    {
+        RaycastHit hit;
+        Ray ray = camHolder.ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawLine(camHolder.transform.position, ray.direction, Color.red, 5f);
+        if (Physics.Raycast(ray, out hit))
+        {
+            Transform objectHit = hit.transform;
+            if (objectHit.CompareTag("Tile"))
+            {
+                objectHit.GetComponent<Renderer>().material = materials[Random.Range(0, materials.Length)];
+            }
+
+        }
+    }
+
+}
