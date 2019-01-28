ed8df326c7e6a3afcfe3a453da856f3cd235d020
SolidHinken <solidhinken@gmail.com>
2019-01-29 01:28:25 +0300

    29.01.19
    Cube randomizer
    Cinemachine
    Camera point switch system

---
+++ Assets/_Scripts/CubeController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+
+    //Material pool
+    public Material[] materials;
+    //Sides references
+    public Transform[] cubeSides;
+    public Transform cuveBottom;
+
+    //Camera reference points
+    public Transform[] cameraPoints;
+    public int activeCameraPoint = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Initialize cube sides
+        foreach (Transform cubeSide in cubeSides)
+        {
+            Material tmpMat = materials[Random.Range(0, materials.Length)];
+            //Grab references to each element per side
+            foreach (Transform child in cubeSide.GetChild(0).GetChild(0))
+            {
+                //Randomize side's elem colorss
+                float matRandomizer = Random.Range(0, 100);
+                if (matRandomizer <= 50)
+                {
+                    //Side color elem
+                    child.GetComponent<Renderer>().material = tmpMat;
+                }
+                else
+                {
+                    //Blank elem
+                    child.GetComponent<Renderer>().material = materials[0];
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+       
+    }
+
+
+    public void MoveCamera(Transform camera)
+    {
+        activeCameraPoint++;
+        if(activeCameraPoint>= cameraPoints.Length)
+        {
+            activeCameraPoint = 0;
+        }
+        camera.SetParent(cameraPoints[activeCameraPoint]);
+        StartCoroutine(StopCamera(camera));
+    }
+
+    public float cameraSpeed = 1f;
+    public IEnumerator StopCamera(Transform camera)
+    {
+        //Smoothly move camera to point
+        while (camera.transform.localPosition != new Vector3(0f,0f,0f))
+        {
+            camera.localPosition = Vector3.Lerp(camera.transform.localPosition, Vector3.zero, cameraSpeed * Time.deltaTime);
+            yield return null;
+        }
+        Debug.Log("REEE");
+    }
+
+}
--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,17 +1,25 @@
-﻿using System.Collections;
+﻿using Cinemachine;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
 
-    public Material[] materials;
-    public Camera camHolder;
+    public Material bottomMaterial;
 
+    //Cinemachine camera holder
+    public Transform camHolder;
+    //Physical Camera ref for rays etc
+    public Camera physicalCam;
+    //Active cube ref
+    public CubeController activeCube;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Debug Initialize camera
+        ChangeCameraState(activeCube.cameraPoints[activeCube.activeCameraPoint]);
 
     }
 
@@ -20,15 +28,21 @@ public class GameManager : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0))
         {
+            //Click on the bottom cube to select
             GameObject tmpObj = GrabRayObj();
-
             if (tmpObj.CompareTag("Tile"))
             {
-                tmpObj.GetComponent<Renderer>().material = materials[Random.Range(0, materials.Length)];
+                tmpObj.GetComponent<Renderer>().material = bottomMaterial;
             }
         }
-        else if(Input.GetMouseButtonDown(1))
+        else if (Input.GetMouseButtonDown(1))
         {
+            //Switch between camera points
+            activeCube.MoveCamera(camHolder);
+        }
+        else if(Input.GetMouseButtonDown(2))
+        {
+            //Debug open cube anim trigger
             GameObject tmpObj = GrabRayObj();
             if (tmpObj.CompareTag("Tile"))
             {
@@ -37,12 +51,21 @@ public class GameManager : MonoBehaviour
         }
     }
 
-
+    //Reset camera transform and set Parent
+    public void ChangeCameraState(Transform cameraParent)
+    {
+        
+        camHolder.SetParent(cameraParent);
+        camHolder.GetComponent<CinemachineVirtualCamera>().m_LookAt = activeCube.transform;
+        camHolder.localRotation = Quaternion.identity;
+        camHolder.localPosition = Vector3.zero;
+        camHolder.localScale = Vector3.one;
+    }
 
     public GameObject GrabRayObj()
     {
         RaycastHit hit;
-        Ray ray = camHolder.ScreenPointToRay(Input.mousePosition);
+        Ray ray = physicalCam.ScreenPointToRay(Input.mousePosition);
 
         Debug.DrawLine(camHolder.transform.position, ray.direction, Color.red, 5f);
         if (Physics.Raycast(ray, out hit))
