989983185470794d705cc884ef7cd2a4e17c9c58
SolidHinken <solidhinken@gmail.com>
2019-02-18 00:53:03 +0300

    18.02.19
    Tower Level Selection
    Canvas
    Integration with Main scene

--- Assets/TowerAimer.cs
+++ Assets/TowerAimer.cs
@@ -2,20 +2,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TowerAimer : MonoBehaviour
 {
-
+    public Camera physicCam;
     public CinemachineVirtualCamera vcam;
     public Transform FollowTarget;
 
-
+    public Transform currentCanvas;
 
     private void Update()
     {
         
         if(Input.touchCount>0)
         {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                if (currentCanvas && currentCanvas.gameObject.activeSelf)
+                {
+                    GameObject tmp = GrabRayObj();
+
+                    if (tmp && tmp.CompareTag("Cube"))
+                    {
+                        StartCoroutine(StopLoadTransition("Main"));
+                    }
+                }
+            }
             if(Input.GetTouch(0).phase == TouchPhase.Ended)
             {
                 Vector3 tmpPos = new Vector3(transform.position.x, FollowTarget.position.y, transform.position.z);
@@ -23,7 +36,84 @@ public class TowerAimer : MonoBehaviour
                 StartCoroutine(StopLook(tmpPos,0.2f));
             }
         }
+
+
     }
+
+
+
+    public IEnumerator StopLoadTransition(string scene)
+    {
+        yield return new WaitForSeconds(0.4f);
+        SceneManager.LoadScene(scene);
+    }
+
+
+
+    //Elem pick
+    public GameObject GrabRayObj()
+    {
+
+
+
+        Vector3 rayPos = Input.mousePosition;
+        rayPos.z = physicCam.farClipPlane;
+
+        rayPos = physicCam.ScreenToWorldPoint(rayPos);
+
+
+
+
+
+        Debug.DrawLine(rayPos, physicCam.transform.position, Color.red, 5f);
+
+        RaycastHit[] hits = Physics.RaycastAll(physicCam.transform.position, rayPos);
+
+        if (hits.Length != 0)
+        {
+            foreach (var hit in hits)
+            {
+                if (hit.collider.CompareTag("Cube"))
+                {
+                    GameObject objectHit = hit.transform.gameObject;
+                    Debug.Log(objectHit.tag);
+                    return objectHit;
+
+                }
+
+            }
+        }
+
+
+        return null;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+       
+        if (other.gameObject.CompareTag("Cube"))
+        {
+            FollowTarget.position = other.transform.position + Vector3.up*0.6f;
+            
+            other.transform.GetChild(1).gameObject.SetActive(true);
+
+            if(currentCanvas && other.transform.GetChild(1) != currentCanvas)
+            {
+                currentCanvas.gameObject.SetActive(false);
+            }
+            currentCanvas = other.transform.GetChild(1);
+
+
+            //vcam.m_Follow = other.transform;
+        }
+    }
+    //private void OnTriggerExit(Collider other)
+    //{
+
+    //    if (other.gameObject.CompareTag("Cube"))
+    //    {
+    //        other.transform.GetChild(1).gameObject.SetActive(false);
+    //    }
+    //}
 
 
 
@@ -39,22 +129,4 @@ public class TowerAimer : MonoBehaviour
         }
 
     }
-
-    private void OnTriggerEnter(Collider other)
-    {
-       
-        if (other.gameObject.CompareTag("Cube"))
-        {
-            FollowTarget.position = other.transform.position + Vector3.up*0.6f;
-            //vcam.m_Follow = other.transform;
-        }
-    }
-    //private void OnTriggerExit(Collider other)
-    //{
-
-    //    if (other.gameObject.CompareTag("Cube"))
-    //    {
-    //        FollowTarget.position = new Vector3(FollowTarget.position.x, transform.position.y, FollowTarget.position.z);
-    //    }
-    //}
 }
